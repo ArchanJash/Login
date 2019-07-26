152c6f41a56727d6ac6db85b462891b1c5cb0393
Archan <archan@klizos.com>
2019-07-26 11:09:43 +0530

    Initial project setup

---
+++ Login/Login.h
@@ -0,0 +1,19 @@
+//
+//  Login.h
+//  Login
+//
+//  Created by iOS Development on 7/26/19.
+//  Copyright © 2019 iOS Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Login.
+FOUNDATION_EXPORT double LoginVersionNumber;
+
+//! Project version string for Login.
+FOUNDATION_EXPORT const unsigned char LoginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Login/PublicHeader.h>
+
+
